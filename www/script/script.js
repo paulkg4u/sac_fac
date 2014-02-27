03772f6945d8ae0b9107ff195cc38b47315b9a60
Paul K.George <paulkottalil20@gmail.com>
2014-02-27 11:04:46 +0530

    final
    final i think on master

---
+++ www/script/script.js
@@ -0,0 +1,244 @@
+window.onload=init;
+	var i = 0;
+	var fact_array =new Array("Praveen Amre bought Sachin Tendulkar his first pair of international quality cricket shoes",
+
+" Sachin used to ask Ramesh Pardhe to dip a rubber ball in water and hurl it at him to know whether he had middled the ball",
+
+" During his school days, Sachin grew his hair and tied a band around it to copy tennis legend John McEnroe",
+
+" Sachin's father named him after the legendary music director Sachin Dev Burman",
+
+" When Sachin was 14, Sunil Gavaskar gave him ultra-light pads. However they were stolen when he was at U15 camp in Indore",
+
+" Dilip Vengsarkar gifted Sachin a Gunn & Moore bat after he was picked for the Mumbai Under-15 team",
+
+" Sachin won 13 coins from his coach Ramakant Achrekar. He won a coin every time he remained unbeaten in the nets",
+
+" Sachin wanted to be a fast bowler but was rejected by Australian great Dennis Lillee at Chennai in 1987",
+
+" Sachin was a ball boy during the 1987 World Cup semi-final between India and England at Wankhede Stadium",
+
+" Gursharan Singh played with one hand (despite a broken finger) to help Sachin get to a century in Irani Cup game of 1989-90",
+
+" Sachin Tendulkar holds the unique distinction of scoring a century on debut in Ranji Trophy, Irani Cup and Duleep Trophy",
+
+" Sachin was returning from England's tour in 1990 when he met his future wife, Anjali, for the first time at Mumbai airport",
+
+" Sachin's father-in-law, Anand Mehta, is a seven-time national champion in Bridge",
+
+" Sachin had to wait for 79 matches for his first ODI century on Sept 9, 1994. By that time he had scored seven Test hundreds",
+
+" Sachin Tendulkar is the only Indian to find a place in Wisden's all-time World XI  ",
+
+" Sachin Tendulkar features in Sir Bradman's all time Test XI, the only player from the current generation  ",
+
+" In the team bus, Sachin Tendulkar always takes the left window seat of the front row  ",
+
+" In 2008, Sachin was conferred Padma Vibhushan – India's second highest civilian honour  ",
+
+" During 2007 Lord's Test, British actor Daniel Radcliffe queued up for an autograph of Sachin at the end of the game  ",
+
+" In 1999 Sachin was conferred \"Padma Shri\" – India's fourth highest civilian honour  ",
+
+" In 1992, Sachin became the first overseas player to represent Yorkshire county team  ",
+
+" London Times' John Woodcock once said: \"He is the best I have seen in my life. And unlike most of you, I have seen Bradman\"",
+
+
+
+"Sachin has scored centuries against all Test playing nations. He was the third batman to achieve the distinction after Steve Waugh and Gary Kirsten.",
+
+"Only batsman to have scored at least TWO centuries against ALL other Test playing countries.",
+
+"The first ball Sachin faced in international cricket was bowled by Waqar Younis. Mohammad Azharuddin was his partner.",
+
+"Sachin's record of five test centuries before he turned 20 is a world record, still held by him",
+
+" Is the only player to score Test centuries more than twenty years apart. Don Bradman's first and last three-figure scores came 19 years 7 months apart.",
+
+"His seven centuries in 2010 are the most by an Indian batsman in a calendar year.",
+
+" 1562 runs scored by Sachin in Test cricket in 2010 are also the most by an Indian batsman in a calendar year.",
+
+"12 of Sachin's Test hundreds have come after the age of 35. England's Graham Gooch remains the only other player to have done so.",
+
+"15 of Sachin's Test centuries came under Mohammad Azharuddin.",
+
+"Tendulkar has the distinction of scoring most fifty-plus innings (51 centuries and 67 fifties = 118) which remains the world record for any batsman.",
+
+"Holds the record of aggregating most runs in AWAY Tests (8705).",
+
+"Is the first batsman to have aggregated 1,000 runs against SEVEN different countries in Test cricket. The only teams missing are Zimbabwe (918 runs) and Bangladesh (820 runs).",
+
+"Holds the record of most runs in between two dismissals with 497 runs in 2003-04 season (spread across three Tests and five innings with the sequence of 241*, 60*,194* and 2).",
+
+"First Indian to score 300 runs in a Test without getting dismissed.",
+
+"Has crossed 150 on 20 occasions in Test cricket – most such instances for any batsman in Test cricket.",
+
+"Has completed his hundred with a six on SIX occasions in Test cricket – a world record.",
+
+"When he scored his 51st hundred, which was against South Africa at Cape Town, he became the first overseas batsman to have scored 5 centuries on South African soil. The previous highest was 4 Test centuries by Wally Hammond of England and Neil Harvey of Australia.",
+
+"Sachin holds the record of aggregating most runs by an overseas batsman in two countries- Sri Lanka and Bangladesh.",
+"Became the first player to surpass the 12,000, 13,000, 14,000 & 15,000 Test run marks.",
+
+"Tendulkar is the fastest to reach the following thousand-run marks in terms of innings: 10000 runs (195 innings- joint record holder with Brian Lara and Kumar Sangakkara), 12000 runs (247 innings - joint record holder with Ricky Ponting),13000 runs (266 innings),14000 runs (279 innings) and 15000 (300 innings).",
+
+"Holds the unique record of having appeared in most WON (70) and most LOST (56) Tests for India.",
+
+"Sachin has been dismissed stumped only once in Test matches. The only occasion was against England at Bangalore in 2002. Just 10 runs away from his century,Sachin got irritated by Ashley Giles' negative bowling (bowling well outside the leg stump) and wicketkeeper James Foster effected a smart stumping.",
+
+" In 2001-02 Sachin was dismissed LBW in five consecutive innings – an Indian record",
+
+"Sachin has been involved in 23 run-outs. He was out on 9 occasions and his partner on 14 occasions.",
+
+"Sachin played ODIs on 96 different grounds – most by any player.",
+
+" If Sachin's final Test goes to the fifth day, Sachin would have a career span of 24 years 3 days- fifth longest for any player in Test annals and longest ever for a sub-continent player.",
+
+"Sachin played his first 32 Tests on different grounds.",
+
+"Sachin's 14 man of the match awards are the most by an Indian in Test cricket.",
+
+"Sachin's 5 man of the series awards are the most by an Indian in Test cricket (along with Virender Sehwag).",
+
+"Sachin Tendulkar and Rahul Dravid put on 20 hundred-plus stands in Test cricket. This is a world record for the most number of century partnerships by a single pair in Test cricket.",
+
+"Despite being dubbed as a unsuccessful captain, averaged 51.35 in 25 matches in which he captained India. His average is the highest for an Indian captain with at least 500 runs to his credit.",
+
+"Sachin has played Test cricket with 593 players (108 teammates and 485 opponents) – most for any player in Test cricket",
+
+"Sachin played 47 of his Tests under the captaincy of Mohammad Azharuddin - more than any other captain",
+
+" Sachin has played 146 Tests with Rahul Dravid, 122 with Anil Kumble, 120 with VVS Laxman and 103 with Sourav Ganguly",
+
+ "Sachin hit 2 sixes off his first two balls in India's fourth innings successful chase against Australia at Chennai in 2013.",
+
+" Eleven bowlers have dismissed Sachin as their first wicket in Tests. They are: Hansie Cronje (South Africa), Ujesh Ranchod (Zimbabwe), Ruwan Kalpage (Sri Lanka), Mark Ealham (England), Neil Johnson (Zimbabwe), Jacob Oram (New Zealand), Monty Panesar (England), Cameron White (Australia), Peter Siddle (Australia), Peter George (Australia) and Andy McKay (New Zealand).",
+
+" Sachin has never batted at number three in his entire Test career. He has opened once, batted 273 times at number 4, 29 times at number 5, 20 times at number 6 and 4 times at number 7.",
+
+" Sachin has top-scored on 53 occasions in a completed innings- more than any other Test player.",
+
+"Between November 1989 and June 2001 Sachin played 84 consecutive Tests without a break.",
+" In August 1987, Sachin was ignored for Bombay Cricket Association's Best Junior Cricketer of the Year award, Sunil Gavaskar then wrote an encouraging letter to the fourteen-year-old with the postscript: ‘Don't be disappointed at not getting the Best Junior cricketer award from BCA. If you look at the best award winners, you will find one name missing and that person has not done badly in Test cricket!'",
+
+
+" He is one of the first sportsmen, along with Grandmaster Vishwanathan Anand to receive Padma Vibhusan in 2008, which is Indian's second highest civilian honour",
+
+" Sachin scored a hundred (100*) for Mumbai against Gujarat at Mumbai in 1988-89 season on his first class debut, to then become the youngest to do so on debut in Indian first class cricket and the second youngest to score a hundred at the age of 15 years & 232 days.",
+
+" He remains the only player to score century on debut in Ranji Trophy, Irani Trophy and Duleep Trophy. His scoring sequence were: 100 not out for Mumbai against Gujarat at Mumbai in 1988-89 in Ranji Trophy, 103 not out for Rest of India against Delhi at Delhi in 1989-90 in Irani Trophy and 159 for West Zone against East Zone at Guwahati in 1990-91 in Duleep Trophy.",
+
+" On his Test debut, Sachin Tendulkar was the third youngest debutant (16 years 205 days). Mushtaq Mohammad (15 years 124 days) and Aaqib Javed (16 years 189 days) debuted in Test matches at a younger age than Tendulkar.",
+
+"When Tendulkar scored his maiden Test century in 1990, he was the second youngest to score a century. Only Mushtaq Mohammad had scored a century at a younger age by 1990. Tendulkar's record was bettered by Mohammad Ashraful in 2001-02 season. The record for previous youngest Indian centurion was held by Kapil Dev.",
+
+
+"Sachin has the distinction of scoring the most runs at any position in Test cricket. In 273 innings, he has amassed 13408 runs at his favourite batting position (# 4) with the help of 44 centuries and 57 fifties.",
+"Highest number of Test centuries (51), overtaking Sunil Gavaskar's record (34) in December 2005 vs Sri Lanka in Delhi.",
+
+" He fielded for Pakistan as a substitute during a one-day practice match against India at Mumbai's Brabourne Stadium in 1988.",
+
+" Playing for his school Sharadashram against St. Xavier's at the Azad Maidan in February, 1988, he was associated in the then world record unbroken stand of 664 runs with Vinod Kambli for the third-wicket. Both players remained unbeaten on 326 and 349 respectively.",
+
+" He sang and whistled with Vinod Kambli during their 664-run record stand in the Harris Shield tournament in 1988 to avoid eye contact with the coach's assistant who wanted to declare while the duo wanted to bat on.",
+" Two wards in Delhi's Tihar Jail were named after Sachin and Vinod Kambli, after the duo shared a 664-run unbroken partnership in a school tournament.",
+
+" Sachin scored a duck on his One Day International debut against Pakistan at Gujranwala on December 18, 1989",
+
+" Sachin's first man of the match in a Test was at Manchester in 1990 and he got Magnum champagne bottle as the prize. Sachin preserved it for eight years and finally uncorked it on his daughter Sara's first birthday.",
+
+" He was without a bat contract during the 1996 World Cup in which he emerged as the highest run-getter.",
+
+" In 1992, he became the first overseas player to represent Yorkshire county team.",
+
+" Aged 19, it made him the youngest Indian to play in county cricket",
+
+" On November 14, 1992, playing against South Africa at Kingsmead in Durban, Tendulkar became the first batsman to have been declared run out by a third umpire.",
+
+" In 1998 Sachin was chosen for the 1997-98 Rajiv Gandhi Khel Ratna Award.",
+
+" When then BCCI president Raj Singh Dungarpur was asked why was Tendulkar not seen at the presentation after Pakistan won the Chennai Test of 1999 despite a brilliant hundred from Tendulkar, a distraught Raj Singh said: \"He is crying in the dressing room.\"",
+
+" Tendulkar refused to do a Pepsi ad because it required him to smash cricket balls with a fly swatter. He told ad-film maker Prahlad Kakkar that this will project him as bigger than the game of cricket. The ad was modified and stumps replaced the fly swatter",
+
+" Launched in 2009 by a company founded by former investment banker Karl Fowler, a book on Sachin Tendulkar - Tendulkar Opus -has 852 pages edged in gold leaf with each page measuring 50cm x 50cm and weighs 37 kg.",
+
+" In 2010, Sachin was conferred an honorary rank of the Indian Air Force which made him the first sportsperson to be conferred a rank by the IAF and the first personality with no aviation background to receive the honour.",
+
+" In the dressing room, he chooses his spot first -Sachin always occupies a corner. Once he has exercised his choice, others rush to take their places.",
+
+" Sachin follows Roger Federer and Formula 1 and understands music and medicine. Is fond of seafood and can hold a conversation on the merits of different wines.",
+
+" The team has a system of monetary fines for players coming late (to the bus or a meeting or a function) and for flouting the dress code. But Tendulkar has never had to pay up in 23 years.",
+
+" Sachin bats right-handed, bowls with his right-arm, but writes with his left hand",
+
+" In 2002 cricket bible Wisden rated him as the second greatest Test batsman, behind Sir Don Bradman",
+
+" In 2003 Wisden rated Sachin the greatest ever ODI player",
+
+" In 2010 he won the ICC Award-Sir Garfield Sobers trophy for cricketer of the year",
+
+" He is the first cricketer to receive Rajiv Khel Ratna, India's highest sporting honour",
+
+" Sachin features in Bradman's all time Test XI, the only player from the current generation",
+
+" Sachin is the only Indian to find a place in Wisden's all-time World XI.",
+" The Don, rated by many as the greatest batsman of all time, considered Tendulkar to have a batting style similar to his. \"Bradman was most taken by Tendulkar's technique, compactness and shot production, and had asked his wife to have a look at Tendulkar, having felt that Tendulkar played like him. Bradman's wife, Jessie, agreed that they did appear similar,\" his biography records",
+
+" Tendulkar has been on Twitter since May 2010, tweeting as @sachin_rt",
+
+" In less than a year Sachin hit the one million follower mark to become the only Indian in the million-follower club.",
+
+" He is the only Indian cricketer to have a waxwork at Madame Tussaud's",
+
+"Sachin Tendulkar with Sourav Ganguly holds the world record for the maximum number of runs scored by the opening partnership. They put together 6609 runs in 136 matches that include 21 century partnerships and 23 fifty run partnerships. The 21 century partnerships for the opening pair is also a world record.",
+"Is the only player with five scores of 150 or more."
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		);
+function init(){
+	
+
+	process();
+
+}
+
+function inci () {
+	if (i<100) {i=i+1;};
+	process();
+}
+function deci () {
+	if (i>0) {i=i-1;};
+	process();
+}
+function process() {
+	
+	var li = document.createElement("li");
+	var ul=document.getElementById("listing");
+	ul.innerHTML="";
+
+		li.innerHTML=fact_array[i];
+		ul.appendChild(li);
+		
+			
+		
+		
+
+}
